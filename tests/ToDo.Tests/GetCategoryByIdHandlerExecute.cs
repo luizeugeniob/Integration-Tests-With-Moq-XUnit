e12c516bf955c70881b4552a6ad6a802fd3b2451
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-10-25 21:44:15 -0300

    Implementado caso de teste para verificar se o método que obtém uma categoria é chamado apenas uma vez, dado um id de categoria existente.

---
+++ tests/ToDo.Tests/GetCategoryByIdHandlerExecute.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using ToDo.Core.Commands;
+using ToDo.Infrastructure;
+using ToDo.Services.Handlers;
+using Xunit;
+
+namespace ToDo.Tests
+{
+    public class GetCategoryByIdHandlerExecute
+    {
+        [Fact]
+        public void WhenExistsIdShouldInvokeGetCategoryByIdOnlyOnceTime()
+        {
+            //Arrange
+            var categoryId = 20;
+            var command = new GetCategoryById(categoryId);
+
+            var mock = new Mock<IToDoTaskRepository>();
+
+            var repository = mock.Object;
+
+            var handler = new GetCategoryByIdHandler(repository);
+
+            //Act
+            handler.Execute(command);
+
+            //Assert
+            mock.Verify(r => r.GetCategoryById(categoryId), Times.Once());
+        }
+    }
+}
